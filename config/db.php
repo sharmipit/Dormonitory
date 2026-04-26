bc87d2dc59bbd4dc97410b32d99c7bba4b5d5fb5
sharmipit <sharmagnegamboa11@gmail.com>
2026-04-26 14:46:47 +0800

    config/db.php added

---
+++ config/db.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "dormonitory_db";
+
+try {
+    $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Database Connection Failed: " . $e->getMessage());
+}
+?>

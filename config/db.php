2e69b1471af784267a9af20ac9f18b12b9819d6a
sharmipit <sharmagnegamboa11@gmail.com>
2026-04-26 15:19:48 +0800

    db.php modified

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "dormitory_db";
+$dbname = "dormonitory_db";
 
 try {
     $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
